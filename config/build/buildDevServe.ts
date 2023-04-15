096cd44f9d08227a880aba999628436112f5663f
cvine <rafarakh@gmail.com>
2023-04-15 18:10:11 +0300

    feat: 🎸 css modules set

--- config/build/buildDevServe.ts
+++
@@ -1,6 +0,0 @@
-import { BuildOptions } from "./types/config";
-import { Configuration as DevServerConfiguration } from "webpack-dev-server";
-
-export function buildDevServer(options: BuildOptions): DevServerConfiguration {
-  return {};
-}
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-//asdfsadf
-
-const x = () => 11;
-
-const arr = [];
-
-document.body.innerHTML = `<h1>hello world</h1>`;
