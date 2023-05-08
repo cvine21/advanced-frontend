48f7bb3408fa275057431800d161605f8ccbce06
cvine <rafarakh@gmail.com>
2023-05-08 13:53:47 +0300

    refactor: 💡 router architecture

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,9 @@
-import { Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 import "./styles/index.scss";
 
+import AppRouter from "app/providers/router";
 import { useTheme } from "app/providers/ThemeProvider";
-
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
 
 import { classNames } from "shared";
 
@@ -19,12 +15,7 @@ const App = () => {
       <button onClick={toggleTheme}>Toggle theme</button>
       <Link to={"/"}>Main</Link>
       <Link to={"/about"}>About</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={"/about"} element={<AboutPage />} />
-          <Route path={"/"} element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
