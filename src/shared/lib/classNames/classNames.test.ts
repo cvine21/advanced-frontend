0f11bd49b02eb8c4139767c99e90bb49cc99e69d
Claudine Vine <rafarakh@gmail.com>
2023-05-27 17:10:29 +0300

    feat: 🎸 jest config

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,46 @@
+import {classNames} from './classNames';
+
+describe('classNames', () => {
+	it('receives only first param', () => {
+		expect(classNames('someClass')).toBe('someClass');
+	});
+
+	it('receives first and additional classes', () => {
+		const expected = 'someClass class1 class2';
+
+		expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(expected);
+	});
+
+	it('receives mods with true value', () => {
+		const expected = 'someClass class1 class2 hovered scrollable';
+
+		expect(
+			classNames('someClass', {hovered: true, scrollable: true}, [
+				'class1',
+				'class2',
+			]),
+		).toBe(expected);
+	});
+
+	it('receives mod with false value', () => {
+		const expected = 'someClass class1 class2 hovered';
+
+		expect(
+			classNames('someClass', {hovered: true, scrollable: false}, [
+				'class1',
+				'class2',
+			]),
+		).toBe(expected);
+	});
+
+	it('receives mod with undefined value', () => {
+		const expected = 'someClass class1 class2 hovered';
+
+		expect(
+			classNames('someClass', {hovered: true, scrollable: undefined}, [
+				'class1',
+				'class2',
+			]),
+		).toBe(expected);
+	});
+});
