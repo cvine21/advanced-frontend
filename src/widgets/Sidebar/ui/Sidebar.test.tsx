aa3229e89a7fc957000be90ac60669b54476f689
Claudine Vine <rafarakh@gmail.com>
2023-06-28 12:34:22 +0300

    fix: 🐛 sidebar tests
    MemoryRouter wrapper added

--- src/widgets/Sidebar/ui/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar.test.tsx
@@ -1,17 +1,17 @@
 import {fireEvent, screen} from '@testing-library/react';
-import renderWithTranslation from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import componentRender from 'shared/config/tests/componentRender/componentRender';
 
 import Sidebar from './Sidebar';
 
 describe('Button', () => {
 	it('renders sidebar', () => {
-		renderWithTranslation(<Sidebar />);
+		componentRender(<Sidebar />);
 
 		expect(screen.getByTestId('sidebar')).toBeInTheDocument();
 	});
 
 	it('collapses sidebar', () => {
-		renderWithTranslation(<Sidebar />);
+		componentRender(<Sidebar />);
 
 		const toggleBtn = screen.getByTestId('sidebar-toggle');
 
